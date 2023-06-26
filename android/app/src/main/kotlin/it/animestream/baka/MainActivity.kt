8d32d7c98dd1d209db9203cf04c07ddaa8591774
aleeeee1 <pace.alessandro05@protonmail.com>
2023-06-26 23:12:49 +0200

    added support for custom signed ca certificates

--- android/app/src/main/kotlin/it/animestream/baka/MainActivity.kt
+++ android/app/src/main/kotlin/it/animestream/baka/MainActivity.kt
@@ -1,11 +1,35 @@
 package it.animestream.baka
 
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.SplashScreen
-import com.zezo357.flutter_meedu_videoplayer.MeeduPlayerFlutterActivity;
+import com.zezo357.flutter_meedu_videoplayer.MeeduPlayerFlutterActivity
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 class MainActivity: MeeduPlayerFlutterActivity() {
+   init {
+       try {
+           val trustAllCerts: Array<TrustManager> = arrayOf<TrustManager>(
+               object : X509TrustManager {
+                   val acceptedIssuers: Array<Any?>?
+                       get() = arrayOfNulls(0)
 
-// override fun provideSplashScreen(): SplashScreen? = SplashView()
+                   override fun checkClientTrusted(certs: Array<X509Certificate?>?, authType: String?) {}
+                   override fun checkServerTrusted(certs: Array<X509Certificate?>?, authType: String?) {}
+                   override fun getAcceptedIssuers(): Array<X509Certificate> {
+                       TODO("Not yet implemented")
+                   }
+               }
+           )
+           val sc: SSLContext = SSLContext.getInstance("SSL")
+           sc.init(null, trustAllCerts, SecureRandom())
+           HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
+           HttpsURLConnection.setDefaultHostnameVerifier(object : HostnameVerifier {
+               override fun verify(arg0: String?, arg1: SSLSession?): Boolean {
+                   return true
+               }
+           })
+       } catch (e: Exception) {
+       }
+   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,24 @@ import 'package:baka_animestream/ui/pages/main_page.dart';
 import 'package:baka_animestream/ui/theme/theme_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:io';
 
 import 'package:baka_animestream/services/internal_db.dart';
 
 import 'package:flutter_meedu_videoplayer/meedu_player.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+
   initMeeduPlayer();
 
   WidgetsFlutterBinding.ensureInitialized();
